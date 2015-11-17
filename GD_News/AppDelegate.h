05f77607f4b85b8e08601b870a19378d83e6eac4
guoda <1023251767@qq.com>
2015-11-17 12:55:12 +0800

    Initial Commit

---
+++ GD_News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GD_News
+//
+//  Created by xiaoyu on 15/11/17.
+//  Copyright © 2015年 guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GD_News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GD_News
+//
+//  Created by xiaoyu on 15/11/17.
+//  Copyright © 2015年 guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
